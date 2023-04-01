ce52cac928499a11c45cd5f1452c7ef143086d68
Martin Funiak <ffuunnii@gmail.com>
2023-04-01 20:18:34 +0200

    New nodejs API with a connection to the DB

--- api/index.ts
+++ api/index.ts
@@ -1,27 +1,29 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 import { Client } from 'pg';
-const client = new Client({
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-})
- 
-client.connect((err) => {
-  if (err) {
-    console.error('connection error', err.stack)
-  } else {
-    console.log('connected')
-  }
-})
- 
-client.query('SELECT * from songs', (err, res) => {
-  console.log(err ? err.stack : res.rows.length);
-  client.end();
-})
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
+
+  const client = new Client({
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+  })
+   
+  client.connect((err) => {
+    if (err) {
+      console.error('connection error', err.stack)
+    } else {
+      console.log('connected')
+    }
+  })
+  
+  client.query('SELECT * from songs') // your query string here
+    .then(result => console.log(result)) // your callback here
+    .catch(e => console.error(e.stack)) // your callback here
+    .then(() => client.end())
+
   const { name = 'World' } = req.query;
   console.log(JSON.stringify(req.query));
   return res.json({
