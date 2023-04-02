6e4c361c9f761004974c3764c7c78f06ad2dffdc
Martin Funiak <ffuunnii@gmail.com>
2023-04-02 22:30:55 +0200

    Adding CORS to the add recording route

--- api/add/recording.ts
+++ api/add/recording.ts
@@ -2,6 +2,29 @@ import type { VercelRequest, VercelResponse } from '@vercel/node';
 import { Client } from 'pg';
 import multiparty from "multiparty";
 
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  return await fn(req, res)
+}
+
+const handler = (req, res) => {
+  const d = new Date()
+  res.end(d.toString())
+}
+
+
 const uploadRecording = async (req: VercelRequest, res: VercelResponse) => {
   const form = new multiparty.Form();
   const data : any = await new Promise((resolve, reject) => {
@@ -15,7 +38,8 @@ const uploadRecording = async (req: VercelRequest, res: VercelResponse) => {
   res.status(200).json({ success: true });
 };
 
-export default uploadRecording;
+module.exports = allowCors(uploadRecording)
+
 export const config = {
   api: {
     bodyParser: false,
