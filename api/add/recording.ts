0d46559f020bbbd040dbec0a48465ead8e5d0427
Martin Funiak <ffuunnii@gmail.com>
2023-04-02 16:01:48 +0200

    Added new API paths

---
+++ api/add/recording.ts
@@ -0,0 +1,29 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+import { Client } from 'pg';
+
+export default function handler(req: VercelRequest, res: VercelResponse) {
+
+  const client = new Client({
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+  });
+   
+  client.connect((err) => {
+    if (err) {
+      console.error('connection error', err.stack)
+    } else {
+      console.log(req.body);
+      console.log(req.query);
+      
+      client.query('SELECT * from recordings') // your query string here
+        .then(result => { return res.json(result.rows); })
+        .catch(e => console.error(e.stack)) // your callback here
+        .then(() => client.end());  
+    }
+  })
+
+  
+}
