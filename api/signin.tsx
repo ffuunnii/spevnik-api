bf371f6f8f1d916168b6391579a3231040389160
Martin Funiak <ffuunnii@gmail.com>
2023-04-03 23:02:00 +0200

    Addded signin path

---
+++ api/signin.tsx
@@ -0,0 +1,28 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+//import { Client } from 'pg';
+
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  return await fn(req, res)
+}
+
+function handler(req: VercelRequest, res: VercelResponse) {
+    console.log(req.body);
+    return res.json({
+        "token": "string",
+        "first_name": "string",
+        "last_name": "string"
+    });  
+}
+
+module.exports = allowCors(handler)
