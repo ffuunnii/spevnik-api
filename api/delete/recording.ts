f19db00fbdd7461539bec9824aa2a310fe63fe75
Martin Funiak <ffuunnii@gmail.com>
2024-09-19 15:05:27 +0200

    Fixing cors issues

--- api/delete/recording.ts
+++ api/delete/recording.ts
@@ -4,7 +4,7 @@ import * as ftp from "basic-ftp";
 
 const allowCors = fn => async (req, res) => {
   res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
   res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version')
   if (req.method === 'OPTIONS') {
